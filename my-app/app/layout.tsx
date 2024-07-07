2f4e49bf037c1fed241e511567a8bd75affbd599
sandyddeveloper <sandyddeveloper@gmail.com>
2024-07-08 00:35:33 +0530

    fix:navbar

---
+++ my-app/app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import Navbar from "../components/Navbar";
+import Footer from "@/components/Footer";
+
+export const metadata: Metadata = {
+  title: "Travel App",
+  description: "Discover and book unforgettable travel experiences with ease on our all-in-one travel app.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        <Navbar/>
+        <main className="relative overflow-hidden">
+          {children}
+        </main>
+        <Footer/>
+      </body>
+    </html>
+  );
+}
---
+++ my-app/app/page.tsx
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+   <main>
+      <h1 className="font-bold">
+      </h1>
+   </main>
+  );
+}
